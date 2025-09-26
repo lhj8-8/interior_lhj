f6c8c44c296451afe0b8cf389ded755655c42db7
SJM-source <han883477@gmail.com>
2025-09-26 14:23:44 +0900

    sub0926

--- sprout/views/main_views.py
+++ sprout/views/main_views.py
@@ -1,6 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 bp = Blueprint ('main',__name__)
 @bp.route('/')
 def index():
-    return '새싹팀 파이팅'
+    return render_template('main.html')
+
+@bp.route("/sub")
+def sub_page():
+    return render_template("sub.html")
